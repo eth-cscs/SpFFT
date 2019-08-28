ef0ca06e993c4f900fb2522c5f2db977b2e9a6cc
Simon Frasch <simon.frasch@cscs.ch>
2019-08-28 15:43:03 +0200

    doc fixes for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ subprocess.call('cd .. ; doxygen', shell=True)
 
 html_theme = "sphinx_rtd_theme"
 #  html_theme = "bootstrap"
-html_theme_path = ["_themes"]
+html_theme_path = []
 
 # -- Project information -----------------------------------------------------
 
@@ -54,7 +54,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -96,7 +96,7 @@ pygments_style = None
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.

bd206d55d26b4b09fec441630bbc05b364816634
Simon Frasch <simon.frasch@cscs.ch>
2019-09-03 14:50:00 +0200

    added optimized local transpose kernel for Tesla V100

--- src/transpose/gpu_kernels/local_transpose_kernels.cu
+++ src/transpose/gpu_kernels/local_transpose_kernels.cu
@@ -27,6 +27,7 @@
  */
 #include <algorithm>
 #include <cassert>
+#include "spfft/config.h"
 #include "gpu_util/gpu_fft_api.hpp"
 #include "gpu_util/gpu_runtime.hpp"
 #include "memory/gpu_array_const_view.hpp"
@@ -38,7 +39,62 @@ namespace spfft {
 // Backward
 // ------------------
 
+#ifdef SPFFT_CUDA
+// kernel optimized for NVIDIA
+
 // Places data from z-sticks into a full 3d grid
+template <typename T>
+__global__ static void transpose_backward_kernel(const GPUArrayConstView1D<int> indices,
+                                                 const GPUArrayConstView2D<T> freqZData,
+                                                 GPUArrayView2D<T> spaceDomainFlat) {
+  // const int z = threadIdx.x + blockIdx.x * blockDim.x;
+  const int stickIndex = threadIdx.x + blockIdx.x * blockDim.x;
+  const auto stickXYIndex = indices(stickIndex);
+
+  if (stickIndex < indices.size()) {
+    for (int z = blockIdx.y; z < freqZData.dim_inner(); z += gridDim.y) {
+      spaceDomainFlat(z, stickXYIndex) = freqZData(stickIndex, z);
+    }
+  }
+}
+
+auto local_transpose_backward(
+    const gpu::StreamType stream, const GPUArrayView1D<int> indices,
+    const GPUArrayView2D<typename gpu::fft::ComplexType<double>::type>& freqZData,
+    GPUArrayView3D<typename gpu::fft::ComplexType<double>::type> spaceDomain) -> void {
+  assert(indices.size() == freqZData.dim_outer());
+  assert(indices.size() <= spaceDomain.dim_inner() * spaceDomain.dim_mid());
+  assert(spaceDomain.dim_outer() == freqZData.dim_inner());
+  const dim3 threadBlock(128);
+  const dim3 threadGrid((freqZData.dim_outer() + threadBlock.x - 1) / threadBlock.x,
+                        std::min(freqZData.dim_inner(), 2160));
+  launch_kernel(transpose_backward_kernel<typename gpu::fft::ComplexType<double>::type>, threadGrid,
+                threadBlock, 0, stream, indices, freqZData,
+                GPUArrayView2D<typename gpu::fft::ComplexType<double>::type>(spaceDomain.data(), spaceDomain.dim_outer(),
+                               spaceDomain.dim_mid() * spaceDomain.dim_inner(), spaceDomain.device_id()));
+}
+
+auto local_transpose_backward(
+    const gpu::StreamType stream, const GPUArrayView1D<int> indices,
+    const GPUArrayView2D<typename gpu::fft::ComplexType<float>::type>& freqZData,
+    GPUArrayView3D<typename gpu::fft::ComplexType<float>::type> spaceDomain) -> void {
+  assert(indices.size() == freqZData.dim_outer());
+  assert(indices.size() <= spaceDomain.dim_inner() * spaceDomain.dim_mid());
+  assert(spaceDomain.dim_outer() == freqZData.dim_inner());
+  const dim3 threadBlock(128);
+  const dim3 threadGrid((freqZData.dim_outer() + threadBlock.x - 1) / threadBlock.x,
+                        std::min(freqZData.dim_inner(), 2160));
+  launch_kernel(transpose_backward_kernel<typename gpu::fft::ComplexType<float>::type>, threadGrid,
+                threadBlock, 0, stream, indices, freqZData,
+                GPUArrayView2D<typename gpu::fft::ComplexType<float>::type>(
+                    spaceDomain.data(), spaceDomain.dim_outer(),
+                    spaceDomain.dim_mid() * spaceDomain.dim_inner(), spaceDomain.device_id()));
+}
+
+#else
+// kernel optimized for AMD
+// (ideal memory access pattern is different)
+
 template <typename T>
 __global__ static void transpose_backward_kernel(const GPUArrayConstView1D<int> indices,
                                                  const GPUArrayConstView2D<T> freqZData,
@@ -85,6 +141,7 @@ auto local_transpose_backward(
                     spaceDomain.data(), spaceDomain.dim_outer(),
                     spaceDomain.dim_mid() * spaceDomain.dim_inner(), spaceDomain.device_id()));
 }
+#endif
 
 // ------------------
 // Forward

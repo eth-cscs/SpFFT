52311760b5abfb5ea32c9488a7b7b83cdcd36a97
Simon Frasch <simon.frasch@cscs.ch>
2019-09-03 17:47:24 +0200

    updated Fortran interface with new functions

--- include/spfft/spfft.f90
+++ include/spfft/spfft.f90
@@ -140,6 +140,97 @@ interface
   end function
 
   !--------------------------
+  !          Grid Float
+  !--------------------------
+  integer(c_int) function spfft_float_grid_create(grid, maxDimX, maxDimY, maxDimZ, &
+      maxNumLocalZColumns, processingUnit, maxNumThreads) bind(C)
+    use iso_c_binding
+    type(c_ptr), intent(out) :: grid
+    integer(c_int), value :: maxDimX
+    integer(c_int), value :: maxDimY
+    integer(c_int), value :: maxDimZ
+    integer(c_int), value :: maxNumLocalZColumns
+    integer(c_int), value :: processingUnit
+    integer(c_int), value :: maxNumThreads
+  end function
+
+  integer(c_int) function spfft_float_grid_create_distributed(grid, maxDimX, maxDimY, maxDimZ, &
+      maxNumLocalZColumns, maxLocalZLength, processingUnit, maxNumThreads,&
+      comm, exchangeType) bind(C, name='spfft_float_grid_create_distributed_fortran')
+    use iso_c_binding
+    type(c_ptr), intent(out) :: grid
+    integer(c_int), value :: maxDimX
+    integer(c_int), value :: maxDimY
+    integer(c_int), value :: maxDimZ
+    integer(c_int), value :: maxNumLocalZColumns
+    integer(c_int), value :: maxLocalZLength
+    integer(c_int), value :: processingUnit
+    integer(c_int), value :: maxNumThreads
+    integer(c_int), value :: comm
+    integer(c_int), value :: exchangeType
+  end function
+
+  integer(c_int) function spfft_float_grid_destroy(grid) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+  end function
+
+  integer(c_int) function spfft_float_grid_max_dim_x(grid, dimX) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: dimX
+  end function
+
+  integer(c_int) function spfft_float_grid_max_dim_y(grid, dimY) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: dimY
+  end function
+
+  integer(c_int) function spfft_float_grid_max_dim_z(grid, dimZ) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: dimZ
+  end function
+
+  integer(c_int) function spfft_float_grid_max_num_local_z_columns(grid, maxNumLocalZColumns) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: maxNumLocalZColumns
+  end function
+
+  integer(c_int) function spfft_float_grid_max_local_z_length(grid, maxLocalZLength) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: maxLocalZLength
+  end function
+
+  integer(c_int) function spfft_float_grid_processing_unit(grid, processingUnit) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: processingUnit
+  end function
+
+  integer(c_int) function spfft_float_grid_device_id(grid, deviceId) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: deviceId
+  end function
+
+  integer(c_int) function spfft_float_grid_num_threads(grid, numThreads) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: numThreads
+  end function
+
+  integer(c_int) function spfft_float_grid_communicator(grid, comm) &
+      bind(C, name="spfft_float_grid_communicator_fortran")
+    use iso_c_binding
+    type(c_ptr), value :: grid
+    integer(c_int), intent(out) :: comm
+  end function
+
+  !--------------------------
   !        Transform
   !--------------------------
   integer(c_int) function spfft_transform_create(transform, grid, processingUnit, &
@@ -163,6 +254,11 @@ interface
     type(c_ptr), value :: transform
   end function
 
+  integer(c_int) function spfft_transform_clone(transform, newTransform) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    type(c_ptr), intent(out) :: newTransform
+  end function
 
   integer(c_int) function spfft_transform_backward(transform, input, &
                                   outputLocation) bind(C)
@@ -219,10 +315,22 @@ interface
     integer(c_int), intent(out) :: offset
   end function
 
+  integer(c_int) function spfft_transform_global_size(transform, globalSize) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_long_long), intent(out) :: globalSize
+  end function
+
   integer(c_int) function spfft_transform_num_local_elements(transform, numLocalElements) bind(C)
     use iso_c_binding
     type(c_ptr), value :: transform
     integer(c_int), intent(out) :: numLocalElements
+  end function
+
+  integer(c_int) function spfft_transform_num_global_elements(transform, numGlobalElements) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_long_long), intent(out) :: numGlobalElements
   end function
 
   integer(c_int) function spfft_transform_device_id(transform, deviceId) bind(C)
@@ -239,6 +347,128 @@ interface
 
   integer(c_int) function spfft_transform_communicator(transform, comm) &
       bind(C, name="spfft_transform_communicator_fortran")
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: comm
+  end function
+
+  !--------------------------
+  !        Transform Float
+  !--------------------------
+  integer(c_int) function spfft_float_transform_create(transform, grid, processingUnit, &
+      transformType, dimX, dimY, dimZ, localZLength, numLocalElements, indexFormat, indices) bind(C)
+    use iso_c_binding
+    type(c_ptr), intent(out) :: transform
+    type(c_ptr), value :: grid
+    integer(c_int), value :: processingUnit
+    integer(c_int), value :: transformType
+    integer(c_int), value :: dimX
+    integer(c_int), value :: dimY
+    integer(c_int), value :: dimZ
+    integer(c_int), value :: localZLength
+    integer(c_int), value :: numLocalElements
+    integer(c_int), value :: indexFormat
+    integer(c_int), dimension(*), intent(in) :: indices
+  end function
+
+  integer(c_int) function spfft_float_transform_destroy(transform) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+  end function
+
+  integer(c_int) function spfft_float_transform_clone(transform, newTransform) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    type(c_ptr), intent(out) :: newTransform
+  end function
+
+  integer(c_int) function spfft_float_transform_backward(transform, input, &
+                                  outputLocation) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    complex(c_double), dimension(*), intent(in) :: input
+    integer(c_int), value :: outputLocation
+  end function
+
+  integer(c_int) function spfft_float_transform_forward(transform, inputLocation, &
+                                  output, scaling) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), value :: inputLocation
+    complex(c_double), dimension(*), intent(out) :: output
+    integer(c_int), value :: scaling
+  end function
+
+  integer(c_int) function  spfft_float_transform_get_space_domain(transform, &
+                                             dataLocation, dataPtr) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), value :: dataLocation
+    type(c_ptr), intent(out) :: dataPtr
+  end function
+
+  integer(c_int) function spfft_float_transform_dim_x(transform, dimX) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: dimX
+  end function
+
+  integer(c_int) function spfft_float_transform_dim_y(transform, dimY) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: dimY
+  end function
+
+  integer(c_int) function spfft_float_transform_dim_z(transform, dimZ) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: dimZ
+  end function
+
+  integer(c_int) function spfft_float_local_z_length(transform, localZLength) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: localZLength
+  end function
+
+  integer(c_int) function spfft_float_transform_local_z_offset(transform, offset) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: offset
+  end function
+
+  integer(c_int) function spfft_float_transform_global_size(transform, globalSize) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_long_long), intent(out) :: globalSize
+  end function
+
+  integer(c_int) function spfft_float_transform_num_local_elements(transform, numLocalElements) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: numLocalElements
+  end function
+
+  integer(c_int) function spfft_float_transform_num_global_elements(transform, numGlobalElements) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_long_long), intent(out) :: numGlobalElements
+  end function
+
+  integer(c_int) function spfft_float_transform_device_id(transform, deviceId) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: deviceId
+  end function
+
+  integer(c_int) function spfft_float_transform_num_threads(transform, numThreads) bind(C)
+    use iso_c_binding
+    type(c_ptr), value :: transform
+    integer(c_int), intent(out) :: numThreads
+  end function
+
+  integer(c_int) function spfft_float_transform_communicator(transform, comm) &
+      bind(C, name="spfft_float_transform_communicator_fortran")
     use iso_c_binding
     type(c_ptr), value :: transform
     integer(c_int), intent(out) :: comm

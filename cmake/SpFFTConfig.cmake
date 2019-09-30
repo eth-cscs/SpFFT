69ba1002bdf5130c7bf1afc0920ef5713a851a1c
Simon Frasch <simon.frasch@cscs.ch>
2019-09-30 14:01:37 +0200

    exported cmake config now contains build settings (#12)

--- cmake/SpFFTConfig.cmake
+++ cmake/SpFFTConfig.cmake
@@ -1,4 +1,6 @@
 
-include("${CMAKE_CURRENT_LIST_DIR}/SpFFTConfigVersion.cmake")
-
-include("${CMAKE_CURRENT_LIST_DIR}/SpFFTTargets.cmake")
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/SpFFTSharedConfig.cmake")
+	include("${CMAKE_CURRENT_LIST_DIR}/SpFFTSharedConfig.cmake")
+else()
+	include("${CMAKE_CURRENT_LIST_DIR}/SpFFTStaticConfig.cmake")
+endif()

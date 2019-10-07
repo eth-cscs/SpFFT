fcc7d473a2655b797a8db1bccc6be4379e29850f
Simon Frasch <simon.frasch@cscs.ch>
2019-10-07 16:11:28 +0200

    missing device guard added to local transforms

--- src/spfft/transform_internal.cpp
+++ src/spfft/transform_internal.cpp
@@ -93,6 +93,9 @@ TransformInternal<T>::TransformInternal(SpfftProcessingUnitType executionUnit,
     } else {
       // GPU
 #if (defined(SPFFT_CUDA) || defined(SPFFT_ROCM))
+      // set device for current thread
+      GPUDeviceGuard(grid_->device_id());
+
       execGPU_.reset(new ExecutionGPU<T>(grid_->num_threads(), param_, grid_->array_host_1(),
                                          grid_->array_host_2(), grid_->array_gpu_1(),
                                          grid_->array_gpu_2(), grid_->fft_work_buffer()));

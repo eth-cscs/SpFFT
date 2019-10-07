1d48310ed8b12c796491d99534869e4d65a1e169
Simon Frasch <simon.frasch@cscs.ch>
2019-10-07 16:04:23 +0200

    fixed device id check in transform creation if compiled with CUDA without devices available

--- src/spfft/transform_internal.cpp
+++ src/spfft/transform_internal.cpp
@@ -46,11 +46,6 @@ TransformInternal<T>::TransformInternal(SpfftProcessingUnitType executionUnit,
                                         std::shared_ptr<GridInternal<T>> grid,
                                         std::shared_ptr<Parameters> param)
     : executionUnit_(executionUnit), grid_(std::move(grid)), param_(std::move(param)) {
-#if defined(SPFFT_CUDA) || defined(SPFFT_ROCM)
-  // set device for current thread
-  GPUDeviceGuard(grid_->device_id());
-#endif
-
   // ----------------------
   // Input Check
   // ----------------------
@@ -120,6 +115,9 @@ TransformInternal<T>::TransformInternal(SpfftProcessingUnitType executionUnit,
     } else {
       // GPU
 #if (defined(SPFFT_CUDA) || defined(SPFFT_ROCM))
+      // set device for current thread
+      GPUDeviceGuard(grid_->device_id());
+
       execGPU_.reset(new ExecutionGPU<T>(grid_->communicator(), grid_->exchange_type(),
                                          grid_->num_threads(), param_, grid_->array_host_1(),
                                          grid_->array_host_2(), grid_->array_gpu_1(),

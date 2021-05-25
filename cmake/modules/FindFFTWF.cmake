b27601a4073dae9e80ca9c161a0aea90dacd22b7
Simon Frasch <simon.frasch@cscs.ch>
2021-05-25 13:36:12 +0200

    Added separate FindFFTWF module for CMake to avoid linking to fftw3f if not required

--- cmake/modules/FindFFTWF.cmake
+++ cmake/modules/FindFFTWF.cmake
@@ -26,73 +26,74 @@
 
 
 #.rst:
-# FindFFTW
+# FindFFTWF
 # -----------
 #
-# This module looks for the fftw3 library.
+# This module looks for the fftw3f library.
 #
 # The following variables are set
 #
 # ::
 #
-#   FFTW_FOUND           - True if double precision fftw library is found
-#   FFTW_LIBRARIES       - The required libraries
-#   FFTW_INCLUDE_DIRS    - The required include directory
+#   FFTWF_FOUND           - True if single precision fftw library is found
+#   FFTWF_LIBRARIES       - The required libraries
+#   FFTWF_INCLUDE_DIRS    - The required include directory
 #
 # The following import target is created
 #
 # ::
 #
-#   FFTW::FFTW
+#   FFTWF::FFTWF
 
 
 
 # set paths to look for library
-set(_FFTW_PATHS ${FFTW_ROOT} $ENV{FFTW_ROOT})
-set(_FFTW_INCLUDE_PATHS)
+set(_FFTWF_PATHS ${FFTW_ROOT} $ENV{FFTW_ROOT} ${FFTWF_ROOT} $ENV{FFTWF_ROOT})
+set(_FFTWF_INCLUDE_PATHS)
 
-set(_FFTW_DEFAULT_PATH_SWITCH)
+set(_FFTWF_DEFAULT_PATH_SWITCH)
 
-if(_FFTW_PATHS)
+if(_FFTWF_PATHS)
     # disable default paths if ROOT is set
-    set(_FFTW_DEFAULT_PATH_SWITCH NO_DEFAULT_PATH)
+    set(_FFTWF_DEFAULT_PATH_SWITCH NO_DEFAULT_PATH)
 else()
     # try to detect location with pkgconfig
     find_package(PkgConfig QUIET)
     if(PKG_CONFIG_FOUND)
-      pkg_check_modules(PKG_FFTW QUIET "fftw3")
+      pkg_check_modules(PKG_FFTWF QUIET "fftw3")
     endif()
-    set(_FFTW_PATHS ${PKG_FFTW_LIBRARY_DIRS})
-    set(_FFTW_INCLUDE_PATHS ${PKG_FFTW_INCLUDE_DIRS})
+    set(_FFTWF_PATHS ${PKG_FFTWF_LIBRARY_DIRS})
+    set(_FFTWF_INCLUDE_PATHS ${PKG_FFTWF_INCLUDE_DIRS})
 endif()
 
 
 find_library(
-    FFTW_LIBRARIES
-    NAMES "fftw3"
-    HINTS ${_FFTW_PATHS}
+    FFTWF_LIBRARIES
+    NAMES "fftw3f"
+    HINTS ${_FFTWF_PATHS}
     PATH_SUFFIXES "lib" "lib64"
-    ${_FFTW_DEFAULT_PATH_SWITCH}
+    ${_FFTWF_DEFAULT_PATH_SWITCH}
 )
-find_path(FFTW_INCLUDE_DIRS
+find_path(FFTWF_INCLUDE_DIRS
     NAMES "fftw3.h"
-    HINTS ${_FFTW_PATHS} ${_FFTW_INCLUDE_PATHS}
+    HINTS ${_FFTWF_PATHS} ${_FFTWF_INCLUDE_PATHS}
     PATH_SUFFIXES "include" "include/fftw"
-    ${_FFTW_DEFAULT_PATH_SWITCH}
+    ${_FFTWF_DEFAULT_PATH_SWITCH}
 )
 
 # check if found
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FFTW REQUIRED_VARS FFTW_INCLUDE_DIRS FFTW_LIBRARIES )
+find_package_handle_standard_args(FFTWF REQUIRED_VARS FFTWF_INCLUDE_DIRS FFTWF_LIBRARIES )
+
 
 # add target to link against
-if(FFTW_FOUND)
-    if(NOT TARGET FFTW::FFTW)
-        add_library(FFTW::FFTW INTERFACE IMPORTED)
+if(FFTWF_FOUND)
+    if(NOT TARGET FFTWF::FFTWF)
+        add_library(FFTWF::FFTWF INTERFACE IMPORTED)
     endif()
-    set_property(TARGET FFTW::FFTW PROPERTY INTERFACE_LINK_LIBRARIES ${FFTW_LIBRARIES})
-    set_property(TARGET FFTW::FFTW PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${FFTW_INCLUDE_DIRS})
+    set_property(TARGET FFTWF::FFTWF PROPERTY INTERFACE_LINK_LIBRARIES ${FFTWF_LIBRARIES})
+    set_property(TARGET FFTWF::FFTWF PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${FFTWF_INCLUDE_DIRS})
 endif()
 
 # prevent clutter in cache
-MARK_AS_ADVANCED(FFTW_FOUND FFTW_LIBRARIES FFTW_INCLUDE_DIRS pkgcfg_lib_PKG_FFTW_fftw3)
+MARK_AS_ADVANCED(FFTWF_FOUND FFTWF_LIBRARIES FFTWF_INCLUDE_DIRS pkgcfg_lib_PKG_FFTWF_fftw3)
